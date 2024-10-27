17ebb67b64907bb8e841b2f5e7a06d20169cc908
Mugalu Wa Thumba Daniel <danielmugalu945@gmail.com>
2024-10-27 16:51:12 +0200

    Creation of the Constructor
    In the new version, from the line 6 to the line 11 the Motorcycle constructor has been initialized.

--- src/Motorcycle.java
+++ src/Motorcycle.java
@@ -3,4 +3,10 @@
 public class Motorcycle extends Vehicle {
     private int engineCapacity;
     private double fuelConsumption; // New attribute
+
+    public Motorcycle(String brand, String model, int year, int engineCapacity, double fuelConsumption) {
+        super(brand, model, year);
+        this.engineCapacity = engineCapacity;
+        this.fuelConsumption = fuelConsumption;
+    }
 }
