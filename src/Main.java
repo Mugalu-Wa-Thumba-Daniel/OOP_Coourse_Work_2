8d25eb40d3e01bf7a2bd8cc0128a5403fe255f4f
Mugalu Wa Thumba Daniel <danielmugalu945@gmail.com>
2024-10-27 17:22:59 +0200

    Creation of objects
    In the new version, from line 3 to 5 we start creating object to ad value to attributes of our classes and we are going to print them

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        Vehicle[] vehicles = new Vehicle[] {
+                new Car("Toyota", "Camry", 2022,300.10, 7.8),
+        };
     }
 }
