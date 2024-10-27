cec31cfcc6cdf46575a338a00068dc0bd845a262
Mugalu Wa Thumba Daniel <danielmugalu945@gmail.com>
2024-10-27 16:34:28 +0200

    Overriding displayDetails Method
    In the new version, from line 12 to 16 the displayDetails() has been initialiwed.
    This method overrides the method in the Vehicle class

--- src/Car.java
+++ src/Car.java
@@ -9,4 +9,9 @@ public class Car extends Vehicle {
         this.maxSpeed = maxSpeed;
         this.fuelConsumption = fuelConsumption;
     }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("\n\t The Car's Brand is \t\t\t: " + getBrand() + "\n\t The Car's Model is \t\t\t: " + getModel() + "\n\t The Car's Maximum Speed is \t: " + maxSpeed);
+    }
 }
