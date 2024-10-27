584ad7b3dc8c1668e5c1dcbd2947c91355340b22
Mugalu Wa Thumba Daniel <danielmugalu945@gmail.com>
2024-10-27 17:04:55 +0200

    Initialization of the constructor
    In the new version, from line 6 to 11 the constructor has been created and initialized

--- src/Truck.java
+++ src/Truck.java
@@ -3,4 +3,10 @@
 public class Truck extends Vehicle {
     private int cargoCapacity;
     private double fuelConsumption; // New attribute
+
+    public Truck(String brand, String model, int year, int cargoCapacity, double fuelConsumption) {
+        super(brand, model, year);
+        this.cargoCapacity = cargoCapacity;
+        this.fuelConsumption = fuelConsumption;
+    }
 }
