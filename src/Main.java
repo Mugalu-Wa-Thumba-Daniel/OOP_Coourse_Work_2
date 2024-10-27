35fb26a1484b5886859ef7d9f05f0ff15e2d94a7
Mugalu Wa Thumba Daniel <danielmugalu945@gmail.com>
2024-10-27 17:24:36 +0200

    Add of the motorcycle object
    In the new version, on the line 5 we have added an object for the Motorcycle class.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Vehicle[] vehicles = new Vehicle[] {
                 new Car("Toyota", "Camry", 2022,300.10, 7.8),
+                new Motorcycle("Honda", "CBR500R", 2022, 5000, 3.5),
         };
     }
 }
