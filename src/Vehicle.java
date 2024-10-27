9be463c2553242d02f946ee8a38995ddb365a3a4
Mugalu Wa Thumba Daniel <danielmugalu945@gmail.com>
2024-10-27 17:45:10 +0200

    Add of the constructor
    In the new version from line 7 to 12 the constructor has been added and initialized

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -4,4 +4,10 @@ public abstract class Vehicle {
     private String brand;
     private String model;
     private int year;
+
+    public Vehicle(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
 }
